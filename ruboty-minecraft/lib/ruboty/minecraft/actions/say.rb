b0a88e93cdabda6488e96f0fe43d890acc58e5b9
Seiei Higa <hanachin@gmail.com>
2014-11-12 20:42:04 +0900

    wakaran

--- ruboty-minecraft/lib/ruboty/minecraft/actions/say.rb
+++ ruboty-minecraft/lib/ruboty/minecraft/actions/say.rb
@@ -6,13 +6,13 @@ module Ruboty
       class Say < Ruboty::Actions::Base
         def call
           message.reply(message[:text])
-          `screen -S minecraft -p 0 -X stuff #{Shellwords.escape(minecraft_command)}`
+          `screen -S minecraft -p 0 -X stuff "#{Shellwords.escape(minecraft_command)}$(printf \\\\r)"`
         rescue => e
           message.reply(e.message)
         end
 
         def minecraft_command
-          %("say #{message[:text]}$(printf \\\\r)")
+          "say #{message[:text]}"
         end
       end
     end
