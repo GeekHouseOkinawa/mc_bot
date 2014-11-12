0e75473d13604f41059fb58fe7dd68faa900ef6e
Seiei Higa <hanachin@gmail.com>
2014-11-12 20:20:33 +0900

    exec!

--- ruboty-minecraft/lib/ruboty/minecraft/actions/say.rb
+++ ruboty-minecraft/lib/ruboty/minecraft/actions/say.rb
@@ -5,7 +5,7 @@ module Ruboty
     module Actions
       class Say < Ruboty::Actions::Base
         def call
-          %(screen -S minecraft -p 0 -X stuff "say #{Shellwords.escape(message[:text])}$(printf \\r)")
+          `screen -S minecraft -p 0 -X stuff "say #{Shellwords.escape(message[:text])}$(printf \\r)"`
         end
       end
     end
