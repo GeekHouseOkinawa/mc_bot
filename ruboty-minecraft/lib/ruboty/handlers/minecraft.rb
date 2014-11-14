b87811bd5b7cd9abeb3b6900964c5f9153a7efb9
Seiei Higa <hanachin@gmail.com>
2014-11-14 13:02:26 +0900

    sayうつのだるい

--- ruboty-minecraft/lib/ruboty/handlers/minecraft.rb
+++ ruboty-minecraft/lib/ruboty/handlers/minecraft.rb
@@ -3,7 +3,7 @@ require "ruboty/minecraft/actions/say"
 module Ruboty
   module Handlers
     class Minecraft < Base
-      on /say (?<text>.*)/, name: 'say', description: 'say minecraft'
+      on /(?<text>.*)/, name: 'say', description: 'say minecraft'
 
       def say(message)
         Ruboty::Minecraft::Actions::Say.new(message).call
